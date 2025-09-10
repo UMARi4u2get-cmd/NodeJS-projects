91a031d814ee4e27cf92a064d1b558d1033da5a6
M. umar Farooq <umar.i4u2get@gmail.com>
2025-09-10 21:13:17 +0500

    add custom module template

--- 2-adding-html-templates/index.js
+++ 2-adding-html-templates/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 // port
 const port = 8000;
 const tempOverview = fs.readFileSync(`${__dirname}/templates/temp-overview.html`, 'utf-8');
@@ -11,22 +13,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/temp-product.html`, 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-  return output;
-};
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
