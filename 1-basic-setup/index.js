5ef24223d5ec5b1330bf807bf627a9e36c661c4d
M. umar Farooq <umar.i4u2get@gmail.com>
2025-09-10 21:22:55 +0500

    ...

--- 1-basic-setup/index.js
+++ 1-basic-setup/index.js
@@ -1,23 +1,20 @@
 const fs = require('fs');
 const http = require('http');
 
-const status_ok = 200;
-const status_pageNotFound = 404;
-
+const port = 8000;
 // const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const port = 8000;
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/review') {
-    res.writeHead(status_ok, { 'content-type': 'text/html' });
+    res.writeHead(200, { 'content-type': 'text/html' });
     res.end('<h1> This is the Home page </h1>');
   } else if (pathName === '/product') {
-    res.writeHead(status_ok, { 'content-type': 'application/json' });
+    res.writeHead(200, { 'content-type': 'application/json' });
     res.end(data);
   } else {
-    res.writeHead(status_pageNotFound, { 'content-type': 'text/html' });
+    res.writeHead(404, { 'content-type': 'text/html' });
     res.end('<h1> page not found </h1>');
   }
 });
